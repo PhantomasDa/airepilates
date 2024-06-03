d639ec79eb3a3a559597c369ec4f6f0ebf07c54f
Christian Mock <chrm666@gmail.com>
2024-06-03 17:32:22 -0500

    ajustes de test no realizado

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    launch: {
+      headless: false, // Cambia a true si prefieres que el navegador no se abra
+      slowMo: 50
+    },
+    browserContext: 'default'
+  };
+  
